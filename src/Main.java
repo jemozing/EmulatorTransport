7ee722490383712ac68ef315d8d495674c94d70f
ponch <ponchikeva@gmail.com>
2023-02-08 01:24:25 +0600

    ConfigData.java can read config.csv file

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         transport_route.add(new Transport(1,1,1,1,"20",1,1,1) {});
         transport_route.get(0).setName("20");
         ConfigData configData = new ConfigData();
-        configData.readConfigFile("config");
+        configData.readConfigFile("config.csv");
         HttpRequest request = new HttpRequest();
         request.GetRequest();
     }
