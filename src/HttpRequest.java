11548435b3f6d9eda42d6900208501bbf89e464a
Jemozing <ponchikeva@gmail.com>
2023-02-07 21:38:45 +0600

    add HttpRequests and methods of working with them

---
+++ src/HttpRequest.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class HttpRequest {
+    private HttpURLConnection con;
+
+    public void PostRequest() throws IOException {
+        var url = "https://httpbin.org/post";
+        var urlParameters = "name=Jack&occupation=programmer";
+        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+
+        try {
+
+            var myurl = new URL(url);
+            con = (HttpURLConnection) myurl.openConnection();
+
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("User-Agent", "Java client");
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            try (var wr = new DataOutputStream(con.getOutputStream())) {
+
+                wr.write(postData);
+            }
+
+            StringBuilder content;
+
+            try (var br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()))) {
+
+                String line;
+                content = new StringBuilder();
+
+                while ((line = br.readLine()) != null) {
+                    content.append(line);
+                    content.append(System.lineSeparator());
+                }
+            }
+
+            System.out.println(content.toString());
+
+        } finally {
+
+            con.disconnect();
+        }
+    }
+    public void GetRequest() throws IOException {
+        var url = "http://webcode.me";
+
+        try {
+
+            var myurl = new URL(url);
+            con = (HttpURLConnection) myurl.openConnection();
+
+            con.setRequestMethod("GET");
+
+            StringBuilder content;
+
+            try (BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()))) {
+
+                String line;
+                content = new StringBuilder();
+
+                while ((line = in.readLine()) != null) {
+
+                    content.append(line);
+                    content.append(System.lineSeparator());
+                }
+            }
+
+            System.out.println(content.toString());
+
+        } finally {
+
+            con.disconnect();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ArrayList<Transport> transport_route = new ArrayList<>();
         transport_route.add(new Transport(1,1,1,1,"20",1,1,1) {});
         transport_route.get(0).setName("20");
         ConfigData configData = new ConfigData();
         configData.readConfigFile("config");
+        HttpRequest request = new HttpRequest();
+        request.GetRequest();
     }
 }
