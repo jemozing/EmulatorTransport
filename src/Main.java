c5164599cc42fefdded54d0b3371ca997462940b
ponch <ponchikeva@gmail.com>
2023-02-17 00:33:46 +0600

    add Thread

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,30 @@ public class Main {
         HttpRequest request = new HttpRequest();
         request.AuthorizationRequest("7 000 000-00-01", "1111");
         DataBaseRequests dataBaseRequests = new DataBaseRequests();
-        dataBaseRequests.readDataBase("C:\\Users\\ponch\\IdeaProjects\\EmulatorTransport\\out\\production\\EmulatorTransport/RouteFiles/mbus_421.csv");
+        Runnable task = () -> {
+            try {
+                dataBaseRequests.readDataBase("C:\\Users\\ponch\\IdeaProjects\\EmulatorTransport\\out\\production\\EmulatorTransport/RouteFiles/mbus_421.csv");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
         //System.out.println(timeDistance(73.3017573000000055571945267729461193084716796875,55.07409440899999708562972955405712127685546875,73.29325199999999540523276664316654205322265625,55.071810999999996738551999442279338836669921875,20));
+        Thread driver = new Driver(task);
+        driver.start();
     }
 
+    public static class Driver extends Thread{
+        private Runnable task;
 
+        public Driver(Runnable task){
+            this.task = task;
+        }
+        @Override
+        public void run() {
+                if(task!= null){
+                    task.run();
+                }
+
+        }
+    }
 }
